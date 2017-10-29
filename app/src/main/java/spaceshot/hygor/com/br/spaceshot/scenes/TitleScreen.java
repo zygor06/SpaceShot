777d383d1b72fb088b30019bebcc73e0101e9941
hygor <hygor_christian@hotmail.com>
2017-10-29 14:37:05 -0200

    Adicinadas as imagens à pasta Assets

--- app/src/main/java/spaceshot/hygor/com/br/spaceshot/scenes/TitleScreen.java
+++ app/src/main/java/spaceshot/hygor/com/br/spaceshot/scenes/TitleScreen.java
@@ -1,18 +1,16 @@
 package spaceshot.hygor.com.br.spaceshot.scenes;
 
-import android.util.Log;
-
 import org.cocos2d.layers.CCLayer;
 import org.cocos2d.layers.CCScene;
+import org.cocos2d.nodes.CCSprite;
 import org.cocos2d.types.CGPoint;
 
-import spaceshot.hygor.com.br.spaceshot.R;
 import spaceshot.hygor.com.br.spaceshot.config.Assets;
 import spaceshot.hygor.com.br.spaceshot.sprites.BackgroundScreen;
 
 import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenHeight;
-import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenWidth;
 import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenResolution;
+import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenWidth;
 
 /**
  * Created by Hygor Dias on 25/10/2017.
@@ -29,7 +27,14 @@ public class TitleScreen extends CCLayer {
                         screenWidth()/2.0f,
                         screenHeight()/2.0f
                 )));
+
+        CCSprite title = CCSprite.sprite(Assets.LOGO);
+        title.setPosition(screenResolution(
+                CGPoint.ccp(screenWidth()/2, screenHeight() - 130)
+        ));
+
         this.addChild(this.background);
+        this.addChild(title);
     }
 
     public CCScene scene(){
