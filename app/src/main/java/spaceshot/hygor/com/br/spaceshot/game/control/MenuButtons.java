782d4d0e281937d5072315ff7f2fa247a2de508b
hygor <hygor_christian@hotmail.com>
2017-10-30 17:46:35 -0200

    Criados os menus e imagens

--- app/src/main/java/spaceshot/hygor/com/br/spaceshot/game/control/MenuButtons.java
+++ app/src/main/java/spaceshot/hygor/com/br/spaceshot/game/control/MenuButtons.java
@@ -1,20 +1,24 @@
 package spaceshot.hygor.com.br.spaceshot.game.control;
 
 import org.cocos2d.layers.CCLayer;
+import org.cocos2d.nodes.CCDirector;
+import org.cocos2d.transitions.CCFadeTransition;
+import org.cocos2d.types.CGPoint;
+
+import spaceshot.hygor.com.br.spaceshot.config.Assets;
+import spaceshot.hygor.com.br.spaceshot.game.scenes.GameScene;
 
 import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenHeight;
 import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenResolution;
 import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenWidth;
-
-import org.cocos2d.types.CGPoint;
-
-import spaceshot.hygor.com.br.spaceshot.config.Assets;
 
 
 public class MenuButtons extends CCLayer implements ButtonDelegate {
 
     private Button playButton;
     private Button highscoredButton;
+    private Button helpButton;
+    private Button soundButton;
 
     public MenuButtons() {
 
@@ -24,10 +28,14 @@ public class MenuButtons extends CCLayer implements ButtonDelegate {
         // Create Buttons
         this.playButton 	  = new Button(Assets.PLAY);
         this.highscoredButton = new Button(Assets.HIGHSCORE);
+        this.helpButton       = new Button(Assets.HELP);
+        this.soundButton      = new Button(Assets.SOUND);
 
         // Set Buttons Delegates
         this.playButton.setDelegate(this);
         this.highscoredButton.setDelegate(this);
+        this.helpButton.setDelegate(this);
+        this.soundButton.setDelegate(this);
 
         // set position
         setButtonspPosition();
@@ -35,6 +43,8 @@ public class MenuButtons extends CCLayer implements ButtonDelegate {
         // Add Buttons to Screen
         addChild(playButton);
         addChild(highscoredButton);
+        addChild(helpButton);
+        addChild(soundButton);
 
     }
 
@@ -43,6 +53,9 @@ public class MenuButtons extends CCLayer implements ButtonDelegate {
         // Buttons Positions
         playButton.setPosition(screenResolution(CGPoint.ccp( screenWidth() /2 , screenHeight() - 250 ))) ;
         highscoredButton.setPosition(screenResolution(CGPoint.ccp( screenWidth() /2 , screenHeight() - 300 ))) ;
+        helpButton.setPosition(screenResolution(CGPoint.ccp( screenWidth() /2 , screenHeight() - 350 )));
+        soundButton.setPosition(screenResolution(CGPoint.ccp( screenWidth() /2 - 100,screenHeight() - 420 )));
+
     }
 
     @Override
@@ -50,16 +63,22 @@ public class MenuButtons extends CCLayer implements ButtonDelegate {
 
         if (sender.equals(this.playButton)) {
             System.out.println("Button clicked: Play");
-
-            // exercicio 3
-            // direcione para a tela do game
-
-
+            CCDirector.sharedDirector().replaceScene(
+                    CCFadeTransition.transition(1.0f,
+                            GameScene.createGame()));
         }
 
         if (sender.equals(this.highscoredButton)) {
             System.out.println("Button clicked: Highscore");
         }
+
+        if (sender.equals(this.helpButton)) {
+            System.out.println("Button clicked: Help");
+        }
+        if (sender.equals(this.soundButton)) {
+            System.out.println("Button clicked: Sound");
+        }
+
 
     }
 
--- app/src/main/java/spaceshot/hygor/com/br/spaceshot/game/scenes/TitleScreen.java
+++ app/src/main/java/spaceshot/hygor/com/br/spaceshot/game/scenes/TitleScreen.java
@@ -3,9 +3,17 @@ package spaceshot.hygor.com.br.spaceshot.game.scenes;
 
 import org.cocos2d.layers.CCLayer;
 import org.cocos2d.layers.CCScene;
+import org.cocos2d.nodes.CCDirector;
+import org.cocos2d.nodes.CCSprite;
+import org.cocos2d.types.CGPoint;
 
+import spaceshot.hygor.com.br.spaceshot.config.Assets;
 import spaceshot.hygor.com.br.spaceshot.game.control.MenuButtons;
 import spaceshot.hygor.com.br.spaceshot.screens.ScreenBackground;
+
+import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenHeight;
+import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenResolution;
+import static spaceshot.hygor.com.br.spaceshot.config.DeviceSettings.screenWidth;
 
 
 public class TitleScreen extends CCLayer {
@@ -19,13 +27,19 @@ public class TitleScreen extends CCLayer {
 	}
 
 	public TitleScreen() {
-			
-		// exercicio 2
-		// background
-		// programe o background aqui
-		
-		// logo
-		// programa o logo aqui
+
+		this.background = new ScreenBackground(Assets.BACKGROUND);
+		this.background.setPosition(
+				screenResolution(CGPoint.ccp(
+						CCDirector.sharedDirector().winSize().width / 2.0f,
+						CCDirector.sharedDirector().winSize().height / 2.0f
+				)));
+		this.addChild(this.background);
+
+		CCSprite title = CCSprite.sprite(Assets.LOGO);
+		title.setPosition(screenResolution(
+				CGPoint.ccp( screenWidth() /2 , screenHeight() - 130 )));
+		this.addChild(title);
 		
 		// Add options layer
 		MenuButtons menuLayer = new MenuButtons();
